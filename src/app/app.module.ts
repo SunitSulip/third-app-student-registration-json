82a968ebb113989e0f94be943d107df07a1d6933
SunitSulip <sunits@mindfiresolutions.com>
2021-03-27 21:27:58 +0530

    initial design of UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
+import { CardComponent } from './card/card.component';
+import { RateComponent } from './rate/rate.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    FormComponent,
+    CardComponent,
+    RateComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
