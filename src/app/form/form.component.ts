b95f70c693e0f613593f57e5c5e1b565de6ee710
SunitSulip <sunits@mindfiresolutions.com>
2021-03-30 18:31:34 +0530

    validations and star rating completed

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  constructor( private fb: FormBuilder) { }
 
-  constructor() { }
+  studentForm = this.fb.group({
+    name :['',Validators.required],
+    gender :['',Validators.required],
+    rollNo : ['',Validators.required],
+    percentage:['',Validators.required],
+    rating :['0']
+  })
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addItem(newItem: string) {
+    this.studentForm.controls['rating'].setValue(newItem);
   }
 
 }
--- src/app/rate/rate.component.ts
+++ src/app/rate/rate.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-rate',
   templateUrl: './rate.component.html',
   styleUrls: ['./rate.component.css']
 })
+// @Output() newItemEvent = new EventEmitter<string>();
 export class RateComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
   }
 
 }
